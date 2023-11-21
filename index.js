e083871e72cef80fe553dd20b23dac8bf996e16d
arifshehab <arifshehab@uchicago.edu>
2023-11-21 23:52:06 +0800

    2nd commit

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import makestruct from "makestruct";
+import yahooFinance from 'yahoo-finance2';
 
 const app = express();
 const port = 3000;
 
 const trades = [];
-const asset = new makestruct("name, position, event, date");
+const asset = new makestruct("name, quantity, event, date");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {data: trades});
   });
   
 app.post("/asset", (req, res) => {
     
-    const newAsset = new asset(req.body["aName"], req.body["aPos"],req.body["aEvent"], req.body["aDate"]);
+    const newAsset = new asset(req.body["aName"], req.body["aQuantity"],req.body["aEvent"], req.body["aDate"]);
     trades.push(newAsset);
-    res.render("addasset.ejs", { addedAssetName: newAsset.name, addedAssetEvent: newAsset.event, addedAssetPos : newAsset.position, addedAssetDate : newAsset.date});
-});  
+    res.render("addasset.ejs", { addedAssetName: newAsset.name, addedAssetEvent: newAsset.event, addedAssetQuantity : newAsset.quantity, addedAssetDate : newAsset.date});
+});
+
+app.post("/", (req, res) => {
+    res.render("index.ejs", {data: trades});
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
